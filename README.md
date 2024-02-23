73a5a57370ff2898b9fb7feeb8438ce430a0b7f2
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-23 16:08:10 +0100

    [add] mail

--- README.md
+++ README.md
@@ -10,10 +10,13 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 ## Svolgimento
+
+MAIL
 1. prompt - chiedi mail ad utente
 2. controllo se c'è la mail utente in mail array
 3. stampa in html mex su esito controllo
 
+DADI
 4. numero random da 1 a 6 del giocatore
 5. numero random da 1 a 6 del computer
 6. decretare il vincitore: chi fa il numero più alto
--- js/script.js
+++ js/script.js
@@ -1,6 +1,62 @@
 
-let mail = prompt ('Quale è la tua mail?')
+let mail = prompt ('Quale è la tua mail?');
 
-const totMail = [a.a@gmail.com, b.b@gmail.com, c.c@gmail.com, d.d@gmail.com, e.e@gmail.com,]
+const totMail = ['a.a@gmail.com', 'b.b@gmail.com', 'c.c@gmail.com', 'd.d@gmail.com', 'e.e@gmail.com'];
 
-if ()
+let verifica = false;
+
+for (let i = 0; i < totMail.length; i++) {
+
+  if (mail == totMail[i]){
+    verifica = true;
+    console.log('trovata');
+
+  }
+}
+
+if (verifica) {
+  alert ('la tua mail è nel sistema');
+  document.querySelector('div').innerHTML='Congratulazioni, hai un dominio che può accedere al sistema! ';
+
+} else {
+  alert ('Mi dispiace! La tua mail NON è nel sistema');
+  document.querySelector('div').innerHTML='Gentile utente, purtroppo hai un dominio che NON può accedere al sistema! ';
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (mail === totMail[i])
+// console.log = mail
